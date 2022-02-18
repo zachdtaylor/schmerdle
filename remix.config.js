0318ac763491fd1f5140ef7eac61356e88be838b
Zach Taylor <zachtylr04@gmail.com>
2022-02-17 22:17:15 -0700

    Initial commit

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  ignoredRouteFiles: [".*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "server/build/index.js",
+  // publicPath: "/build/",
+  // devServerPort: 8002,
+};
